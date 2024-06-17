6a7fc2a8142b4dbc9b5f1a3ae1bfb6564c30c58d
Ângelo Vinicius Hernandez Mazarin <angelotrab22@gmail.com>
2024-06-16 23:11:12 -0300

    a

--- src/main/java/App/controller/LocarQuadraController.java
+++ src/main/java/App/controller/LocarQuadraController.java
@@ -1,4 +1,32 @@
 package App.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+
 public class LocarQuadraController extends Controller{
+
+    @FXML
+    private Button btn_backto_locatario;
+
+    @FXML
+    private Button btn_salvar_locacao;
+
+    @FXML
+    private DatePicker input_data;
+
+    @FXML
+    private ComboBox<?> input_hora;
+
+    @FXML
+    private Label login_tittle;
+
+    @FXML
+    void locar(ActionEvent event) {
+
+    }
+
 }
--- src/main/java/App/model/LocacaoQuadra/LocacaoQuadra.java
+++ src/main/java/App/model/LocacaoQuadra/LocacaoQuadra.java
@@ -10,16 +10,20 @@ public class LocacaoQuadra {
     int id;
     QuadraEsportiva quadra;
     Locatario locatario;
-    LocalDateTime dataHoraInicio;
-    LocalDateTime dataHoraFim;
-    public LocacaoQuadra(QuadraEsportiva quadra, Locatario locatario, LocalDateTime dataHoraInicio, LocalDateTime dataHoraFim) {
+    LocalDateTime dataInicio;
+    LocalDateTime dataFim;
+    String horaInicio;
+    String horaFim;
+    public LocacaoQuadra(QuadraEsportiva quadra, Locatario locatario, LocalDateTime dataInicio, LocalDateTime dataFim, String horaInicio, String horaFim) {
         if (quadra.isDisponivel() == 0) {
             throw new IllegalArgumentException("A quadra não está disponível para locação.");
         }
+        this.horaInicio = horaInicio;
+        this.horaFim = horaFim;
         this.quadra = quadra;
         this.locatario = locatario;
-        this.dataHoraInicio = dataHoraInicio;
-        this.dataHoraFim = dataHoraFim;
+        this.dataInicio = dataInicio;
+        this.dataFim = dataFim;
     }
 
     public int getId() {
@@ -46,25 +50,41 @@ public class LocacaoQuadra {
         this.locatario = locatario;
     }
 
-    public LocalDateTime getDataHoraInicio() {
-        return dataHoraInicio;
+    public LocalDateTime getDataInicio() {
+        return dataInicio;
     }
 
-    public void setDataHoraInicio(LocalDateTime dataHoraInicio) {
-        this.dataHoraInicio = dataHoraInicio;
+    public void setDataInicio(LocalDateTime dataHoraInicio) {
+        this.dataInicio = dataHoraInicio;
     }
 
-    public LocalDateTime getDataHoraFim() {
-        return dataHoraFim;
+    public LocalDateTime getDataFim() {
+        return dataFim;
     }
 
     public void setDataHoraFim(LocalDateTime dataHoraFim) {
-        this.dataHoraFim = dataHoraFim;
+        this.dataFim = dataHoraFim;
     }
 
     public double calcularValor() {
-        long horas = java.time.Duration.between(dataHoraInicio, dataHoraFim).toHours();
+        long horas = java.time.Duration.between(dataInicio, dataFim).toHours();
         return horas * quadra.getPrecoPorHora();
+    }
+
+    public String getHoraFim() {
+        return horaFim;
+    }
+
+    public void setHoraFim(String horaFim) {
+        this.horaFim = horaFim;
+    }
+
+    public String getHoraInicio() {
+        return horaInicio;
+    }
+
+    public void setHoraInicio(String horaInicio) {
+        this.horaInicio = horaInicio;
     }
 
     @Override
@@ -72,8 +92,8 @@ public class LocacaoQuadra {
         return "LocacaoQuadra{" +
                 "quadra=" + quadra +
                 ", locatario='" + locatario + '\'' +
-                ", dataHoraInicio=" + dataHoraInicio +
-                ", dataHoraFim=" + dataHoraFim +
+                ", dataHoraInicio=" + dataInicio +
+                ", dataHoraFim=" + dataFim +
                 ", valor=" + calcularValor() +
                 '}';
     }
